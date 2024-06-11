039bdcd04bf064dfe41e10072707ca4b5a8c4c03
Shelabh <68127067+shelabh@users.noreply.github.com>
2024-06-12 00:10:49 +0530

    added navbar

--- pages/Landing/Landing.tsx
+++ pages/Landing/Landing.tsx
@@ -4,6 +4,11 @@
 const Landing = () => {
 	return (
 		<>
+			<div className="bg-[#47EDA8] h-screen flex flex-col items-center justify-center ">
+				<div className="text-3xl font-bold text-[#121811]">
+					hello
+				</div>
+			</div>
 		</>
 	)
 }
---
+++ pages/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+const Navbar = () => {
+	return (
+		<>
+			<div className="flex flex-row justify-between items-center fixed bg-white text-[#121811] py-4 px-10 w-full">
+				<div className="text-xl font-semibold">
+					PAPA
+				</div>
+				<div className="border-2 rounded-md w-28 flex flex-row justify-center p-2 border-[#00373D]   font-medium">
+					JOIN
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Navbar
